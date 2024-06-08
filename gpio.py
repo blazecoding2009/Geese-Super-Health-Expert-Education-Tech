cdb2d60eb93fc9bf6598d4c51e71e8433df863cf
blaze <Arnnav0kudale@gmail.com>
2024-06-08 18:57:21 -0400

    work istg

--- gpio.py
+++ gpio.py
@@ -106,7 +106,7 @@ def flash(pin):
     thread.start()
     flashing_threads[pin] = thread
 
-    return jsonify({pin: 'flashing', 'interval': interval, 'duration': duration})
+    return jsonify({pin: 'flashing', 'interval': str(interval), 'duration': str(duration)})
 
 @app.route('/gpio/<int:pin>/stop_flash', methods=['POST'])
 def stop_flash(pin):
